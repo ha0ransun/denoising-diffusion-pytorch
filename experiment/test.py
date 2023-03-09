988234a71a1c5500f879753f07bbffff133329cd
riemannnn@gmail.com <riemannnn@gmail.com>
2023-03-08 18:24:47 -0800

    update cov entropy

--- experiment/test.py
+++ experiment/test.py
@@ -31,7 +31,24 @@ def get_all_entropy(x, num, batch_size):
     all_entropy = torch.zeros(num, device=x.device)
     for i in tqdm(range(int(np.ceil(x.shape[0] / batch_size)))):
         cur_x = x[i * batch_size: (i + 1) * batch_size]
-        cur_d = (torch.cdist(cur_x, x) ** 2) * 4 / 255 ** 2
+        cur_d = torch.round((torch.cdist(cur_x, x) ** 2) * 4 / 255 ** 2, decimals=6)
+        for j in range(num):
+            sigma = (j + 1) / num
+            all_entropy[j] += get_entropy_lb(cur_d, sigma).float()
+    all_entropy /= x.shape[0]
+    return all_entropy
+
+
+def get_cov_entropy(x, num, batch_size):
+    cov = torch.cov(x.T * 2 / 255) * 0.999 + 0.001 * torch.eye(x.shape[-1], device=x.device)
+    cov /= (torch.logdet(cov) / 3072).exp()
+    v, P = torch.linalg.eigh(cov)
+    W = P @ torch.diag(v ** -0.5) @ P.T
+    x_W = torch.round(x @ W, decimals=3)
+    all_entropy = torch.zeros(num, device=x.device)
+    for i in tqdm(range(int(np.ceil(x.shape[0] / batch_size)))):
+        cur_x = x_W[i * batch_size: (i + 1) * batch_size]
+        cur_d = torch.round((torch.cdist(cur_x, x_W) ** 2) * 4 / 255 ** 2, decimals=6)
         for j in range(num):
             sigma = (j + 1) / num
             all_entropy[j] += get_entropy_lb(cur_d, sigma).float()
@@ -46,9 +63,13 @@ if __name__ == '__main__':
     x = x.reshape(x.shape[0], -1)
     x = x.double()
     num = 1000
-    # res = torch.arange(200) + torch.randn(num)
     res = get_all_entropy(x, num=num, batch_size=1000).cpu().numpy()
-    with open('results/entropy.pkl', 'wb') as file:
+    with open('results/entropy_isotropic.pkl', 'wb') as file:
         pickle.dump(res, file)
     plt.plot(range(num), res)
-    plt.savefig('results/entropy.png')
+    plt.savefig('results/entropy_isotropic.png')
+    res = get_cov_entropy(x, num=num, batch_size=1000).cpu().numpy()
+    with open('results/entropy_cov.pkl', 'wb') as file:
+        pickle.dump(res, file)
+    plt.plot(range(num), res)
+    plt.savefig('results/entropy_cov.png')
