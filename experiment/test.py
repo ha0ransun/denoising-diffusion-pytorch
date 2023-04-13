db1b47a91cea68c18dcbf8560886ab9f003cd16c
riemannnn@gmail.com <riemannnn@gmail.com>
2023-04-13 16:38:49 -0700

    new schedule

--- experiment/test.py
+++ experiment/test.py
@@ -22,7 +22,6 @@ def get_entropy(x, weight, sigma):
 
 
 def get_entropy_lb(distance, sigma):
-    distance = distance * (1 - sigma ** 2)
     h = torch.logsumexp(torch.clamp(- distance / (4 * sigma ** 2), min=-200.), -1).sum()
     return h
 
@@ -31,16 +30,16 @@ def get_all_entropy(x, num, batch_size):
     all_entropy = torch.zeros(num, device=x.device)
     for i in tqdm(range(int(np.ceil(x.shape[0] / batch_size)))):
         cur_x = x[i * batch_size: (i + 1) * batch_size]
-        cur_d = torch.round((torch.cdist(cur_x, x) ** 2) * 4 / 255 ** 2, decimals=6)
+        cur_d = torch.round((torch.cdist(cur_x, x) ** 2) / 127.5 ** 2, decimals=6)
         for j in range(num):
-            sigma = (j + 1) / num
+            sigma = 0.002 + j * (80 - 0.002) / (num - 1)
             all_entropy[j] += get_entropy_lb(cur_d, sigma).float()
     all_entropy /= x.shape[0]
     return all_entropy
 
 
 def get_cov_entropy(x, num, batch_size):
-    cov = torch.cov(x.T * 2 / 255) * 0.999 + 0.001 * torch.eye(x.shape[-1], device=x.device)
+    cov = torch.cov(x.T / 127.5) * 0.999 + 0.001 * torch.eye(x.shape[-1], device=x.device)
     cov /= (torch.logdet(cov) / 3072).exp()
     v, P = torch.linalg.eigh(cov)
     W = P @ torch.diag(v ** -0.5) @ P.T
@@ -62,14 +61,14 @@ if __name__ == '__main__':
     x = torch.tensor(dataset.data).to(device)
     x = x.reshape(x.shape[0], -1)
     x = x.double()
-    num = 1000
+    num = 100
     res = get_all_entropy(x, num=num, batch_size=1000).cpu().numpy()
     with open('results/entropy_isotropic.pkl', 'wb') as file:
         pickle.dump(res, file)
     plt.plot(range(num), res)
     plt.savefig('results/entropy_isotropic.png')
-    res = get_cov_entropy(x, num=num, batch_size=1000).cpu().numpy()
-    with open('results/entropy_cov.pkl', 'wb') as file:
-        pickle.dump(res, file)
-    plt.plot(range(num), res)
-    plt.savefig('results/entropy_cov.png')
+    # res = get_cov_entropy(x, num=num, batch_size=1000).cpu().numpy()
+    # with open('results/entropy_cov.pkl', 'wb') as file:
+    #     pickle.dump(res, file)
+    # plt.plot(range(num), res)
+    # plt.savefig('results/entropy_cov.png')
